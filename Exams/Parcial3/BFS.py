a073ac909e0926ce4f03b17e5657cff107c19b7b
MateoG404 <mgutierrezca@unal.edu.co>
2023-06-25 11:03:31 -0500

    SCC, taller 3, update BFS, DFS

--- Exams/Parcial3/BFS.py
+++ Exams/Parcial3/BFS.py
@@ -7,7 +7,8 @@ class Graph:
     
     def add(self, a, b):
         self.graph[a].append(b)
-    
+        self.graph[b].append(a)
+            
     def bfs(self, s, value):
 
         visited = [False] * (max(self.graph) + 1)
@@ -19,10 +20,8 @@ class Graph:
         queue.append(s)
         
         visited[s] = True
-        
 
         while queue:
-        
             current_node = queue.popleft()
             
             if current_node == value:
@@ -35,48 +34,64 @@ class Graph:
                     visited[neighbor] = True
                     parent[neighbor] = current_node
                 
-        
         if path_found:
             path = []
             node = value
-            print(node)
             while node != s:
                 path.append(node)
                 node = parent[node]
-                print(node)
             path.append(s)
             path.reverse()
             return path
         else:
             return None
 
+    def print_bfs(self, s, value):
+        path = self.bfs(s, value)
+        
+        if path:
+            print(f"Ruta más rápida desde {s} hasta {value}: {' -> '.join(map(str, path))}")
+        else:
+            print(f"No se encontró una ruta desde {s} hasta {value}.")
+
+    def BFS(self, s):
+ 
+        # Mark all the vertices as not visited
+        visited = [False] * (max(self.graph) + 1)
+        
+        # Create a queue for BFS
+        queue = []
+ 
+        # Mark the source node as
+        # visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+ 
+        while queue:
+ 
+            # Dequeue a vertex from
+            # queue and print it
+            s = queue.pop(0)
+            print(s, end=" ")
+ 
+            # Get all adjacent vertices of the
+            # dequeued vertex s.
+            # If an adjacent has not been visited,
+            # then mark it visited and enqueue it
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
 # Ejemplo de uso
 graph = Graph()
 
-graph.add(0, 1)
-graph.add(0, 2)
-graph.add(1, 0)
-graph.add(1, 4)
-graph.add(1, 5)
-graph.add(2, 0)
-graph.add(2, 3)
-graph.add(2, 5)
-graph.add(2, 4)
-graph.add(3, 2)
-graph.add(3, 5)
-graph.add(4, 2)
-graph.add(4, 1)
-graph.add(4, 5)
-graph.add(5, 1)
-graph.add(5, 4)
-graph.add(5, 2)
-graph.add(5, 3)
+graph.add(0,1)
+graph.add(0,2)
+graph.add(1,3)
+graph.add(2,4)
+graph.add(4,5)
+graph.add(3,5)
 
-value = 4
 
-path = graph.bfs(0, 4)
-
-if path:
-    print(f"Ruta más rápida desde 0 hasta {value}: {' -> '.join(map(str, path))}")
-else:
-    print(f"No se encontró una ruta desde 0 hasta {value}.")
+graph.BFS(0)
--- Exams/Parcial3/DFS.py
+++ Exams/Parcial3/DFS.py
@@ -36,25 +36,15 @@ class Graph :
         self.DFSUtil(v, visited)
 
 # Ejemplo de uso
-graph = Graph()
+g = Graph()
 
-graph.add(0, 1)
-graph.add(0, 2)
-graph.add(1, 0)
-graph.add(1, 4)
-graph.add(1, 5)
-graph.add(2, 0)
-graph.add(2, 3)
-graph.add(2, 5)
-graph.add(2, 4)
-graph.add(3, 2)
-graph.add(3, 5)
-graph.add(4, 2)
-graph.add(4, 1)
-graph.add(4, 5)
-graph.add(5, 1)
-graph.add(5, 4)
-graph.add(5, 2)
-graph.add(5, 3)
 
-graph.DFS(3)
+
+g.add(1, 0)
+g.add(0, 2)
+g.add(2, 1)
+g.add(0, 3)
+g.add(3, 4)
+
+
+g.DFS(0)
